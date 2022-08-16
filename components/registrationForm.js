5e210c9b5df0b23a5a7b9cdcc7b516dcfc2dcab3
Belousov Dmitriy <d-belousov@hotmail.com>
2022-08-16 21:19:25 +0300

    Build: express
    * Настроена сборка в mode production ( не до конца, толком сборка не работает)

--- components/registrationForm.js
+++ components/registrationForm.js
@@ -15,6 +15,7 @@ export class RegistrationForm extends LitElement {
                 ${tmpl_input({name: "display_name", class: "form__input form__input_login",placeholder: "Логин", type: 'text'})}
                 ${tmpl_input({name: "name", class: "form__input form__input_name",placeholder: "Имя", type: 'text'})}
                 ${tmpl_input({name: "second_name", class: "form__input form__input_second-name",placeholder: "Фамилия", type: 'text'})}
+                ${tmpl_input({name: "display_name", class: "form__input form__input_display-name",placeholder: "Имя и Фамилия", type: 'text'})}
                 ${tmpl_input({name:"phone", class: "form__input form__input_phone",placeholder: "(0)-000-000-00-00", type: 'tel'})}
                 ${tmpl_input({name:"password", class: "form__input form__input_password",placeholder: "******", type: 'text'})}
                 ${tmpl_input({name:"password", class: "form__input form__input_password-again",placeholder: "******", type: 'text'})}
