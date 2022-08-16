91b8e75e98965fd273e2239d91ecfda6cae01ea6
Belousov Dmitriy <d-belousov@hotmail.com>
2022-08-16 09:21:52 +0300

    FIX: Error
    * Номер ошибки для страницы вынесен в метод render
    * Исправленны стили для компонента error

--- components/error.js
+++ components/error.js
@@ -1,24 +1,26 @@
 import {LitElement, html} from "lit";
 import tmpl_text from "../tamplates/tmpl_text";
+import tmpl_button from "../tamplates/tmpl_button";
 
 export class Error extends LitElement{
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
     }
 
-    render() {
+    render(number) {
         return html`
             ${tmpl_text({
-                text: '404',
+                text: number,
                 classWrapper: 'error__text error__text_number'                
             })}
             ${tmpl_text({
                 text: 'Мы уже фиксим',
                 classWrapper: 'error__text error__text_answer'
             })}
-            ${tmpl_text({
+            ${tmpl_button({
                 text:'Назад к чатам',
-                classWrapper: 'error__text error__text_link'
+                class: 'error__text error__text_link',
+                href: 'main.html'
             })}
         <div class="error>        
         </div>`
--- pages/error404.js
+++ pages/error404.js
@@ -1,10 +1,10 @@
 import {html, render} from "lit";
 import {Error} from "../components/error";
 
-const error=new Error()
-const container=document.getElementById('error')
+const error=new Error(404)
+const container=document.querySelector('.error')
 const element=html`<div class="error-fourhunder">
-    ${error.render()}
+    ${error.render('404')}
 </div>`
 
 render(element, container)
