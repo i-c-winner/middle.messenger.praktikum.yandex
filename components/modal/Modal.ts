55e19828f4b851135cd8390315a1b28d31010cdd
Belousov Dmitriy <d-belousov@hotmail.com>
2022-09-09 17:43:45 +0300

    Fix: Modal.ts нет any

--- components/modal/Modal.ts
+++ components/modal/Modal.ts
@@ -6,17 +6,15 @@ type ModalProps= {
   parentId: string,
 }
 class Modal extends AbstractComponent<ModalProps> {
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  private instance: any;
-  private static instance: any;
+
+  static instance: Modal;
 
   constructor(props: ModalProps) {
     super(props);
     if (Modal.instance){
       return Modal.instance
     } else {
-      Modal.prototype.instance=this
+      Modal.instance=this
       return this
     }
   }
